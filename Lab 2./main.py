69915740d774b4978f58b0b09c47d23b8b56c703
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2018-04-09 22:20:43 +0300

    Added not working lab 2

---
+++ Lab 2./main.py
@@ -0,0 +1,175 @@
+from data_getters import Data
+from methods import Runge_Kutta_IV_3var_1step, simpson, dichotomy
+from math import pi
+from scipy import arange
+
+
+# FIXME пофиксить вычисление тока
+
+def get_To_m(I):
+
+    table_I_T0 = Data.get_table_I_T0()
+    len_I_T0 = len(table_I_T0)
+
+    In, Ik = 0, 1
+
+    if I > 1200:
+        In, Ik = len_I_T0 - 2,  len_I_T0 - 1
+
+    for i in range(len_I_T0 - 1):
+        if table_I_T0[i][0] < I < table_I_T0[i + 1][0]:
+            In = i
+            Ik = i + 1
+
+    X0 = table_I_T0[In][0]
+    X1 = table_I_T0[Ik][0]
+    Y0 = table_I_T0[In][1]
+    Y1 = table_I_T0[Ik][1]
+    To = Y0 + (Y1 - Y0) * (I - X0) / (X1 - X0)
+    Y0 = table_I_T0[In][2]
+    Y1 = table_I_T0[Ik][2]
+    m = Y0 + (Y1 - Y0) * (I - X0) / (X1 - X0)
+    return To, m
+
+
+def get_T(I, r):
+    To, m = get_To_m(I)
+    #print((Data.T_w - To), (r / Data.Rad), m)
+    return (Data.T_w - To) * (r / Data.Rad) ** m + To
+
+
+def get_sigma(I, r, p):
+    sigma_table = Data.get_table_sigma()
+    len_sigma = len(sigma_table)
+
+    T = get_T(I, r)
+
+    Tn, Tk = 0, 1
+    if T > 20000:
+        Tn, Tk = len_sigma - 2, len_sigma - 1
+
+    for i in range(len_sigma - 1):
+        if sigma_table[i][0] < T < sigma_table[i + 1][0]:
+            Tn = i
+            Tk = i + 1
+
+    pn, pk = 0, 1
+    len_p = len(Data.p)
+    if p > 25:
+        pn, pk = len_p - 2, len_p - 1
+
+    for i in range(len_p - 1):
+        if Data.p[i] < p < Data.p[i + 1]:
+            pn = i
+            pk = i + 1
+
+    X0 = sigma_table[Tn][0]
+    X1 = sigma_table[Tk][0]
+    Y0 = sigma_table[Tn][pn]
+    Y1 = sigma_table[Tk][pn]
+    fy1 = Y0 + (Y1 - Y0) * (T - X0) / (X1 - X0)
+
+    Y0 = sigma_table[Tn][pk]
+    Y1 = sigma_table[Tk][pk]
+    fy2 = Y0 + (Y1 - Y0) * (T - X0) / (X1 - X0)
+
+    Y0 = Data.p[pn]
+    Y1 = Data.p[pk]
+    sigma = fy1 + (fy2 - fy1) * (p - Y0) / (Y1 - Y0)
+    return sigma
+
+
+def get_nt(I, r, p):
+    table_nt = Data.get_table_nt()
+    len_nt = len(table_nt)
+
+    T = get_T(I, r)
+
+    Tn, Tk = 0, 1
+    if T > 15000:
+        Tn, Tk = len_nt - 2, len_nt - 1
+
+    for i in range(len(table_nt) - 1):
+        if table_nt[i][0] < T < table_nt[i + 1][0]:
+            Tn = i
+            Tk = i + 1
+
+    pn, pk = 0, 1
+
+    len_p = len(Data.p)
+    if p > 25:
+        pn, pk = len_p - 2, len_p - 1
+
+    for i in range(len_p - 1):
+        if Data.p[i] < p < Data.p[i + 1]:
+            pn = i
+            pk = i + 1
+
+    X0 = table_nt[Tn][0]
+    X1 = table_nt[Tk][0]
+    Y0 = table_nt[Tn][pn]
+    Y1 = table_nt[Tk][pn]
+    fy1 = Y0 + (Y1 - Y0) * (T - X0) / (X1 - X0)
+
+    Y0 = table_nt[Tn][pk]
+    Y1 = table_nt[Tk][pk]
+    fy2 = Y0 + (Y1 - Y0) * (T - X0) / (X1 - X0)
+
+    Y0 = Data.p[pn]
+    Y1 = Data.p[pk]
+    nt = fy1 + (fy2 - fy1) * (p - Y0) / (Y1 - Y0)
+    return nt
+
+
+def nt_integrand(r, I, p):
+    nt = get_nt(I, r, p)
+    return nt*r
+
+
+def p_func(p, I):
+    return 2/(Data.Rad * Data.Rad) * simpson(nt_integrand, 0, Data.Rad, 10, *[I, p]) - Data.p_0 * 7242 / Data.T_0
+
+
+def sigma_integrand(r, I):
+    p = dichotomy(p_func, min(Data.p), max(Data.p), Data.eps, *[I])
+    sigma = get_sigma(I, r, p)
+    return sigma * r
+
+
+def get_Rp(t, I):
+    return Data.l_e / (2 * pi * simpson(sigma_integrand, 0, Data.Rad, 10, *[I]))
+
+
+def dI_dt(t, Uc, I):
+    Rp = get_Rp(t, I)
+    return (Uc - (Data.R_k + Rp) * I) / Data.L_k
+
+
+def dUc_dt(t, Uc, I):
+    return -1 / Data.C_k * I
+
+
+if __name__ == '__main__':
+    ts = list(arange(Data.t_0, Data.t_n + Data.tau, Data.tau))
+    I, Uc, Rp = [Data.I_0], [Data.U_0], []
+
+    Ii = Data.I_0
+    Ui = Data.U_0
+    Rpi = get_Rp(Data.t_0, Ii)
+
+    for t in ts[1:]:
+
+        #print(t, Ii, Ui, Rpi)
+        I_next, Uc_next = Runge_Kutta_IV_3var_1step(dI_dt, dUc_dt, t, Ui, Ii, Data.tau)
+        Rpi = get_Rp(t, Ii)
+
+        I.append(I_next)
+        Uc.append(Uc_next)
+        Rp.append(Rpi)
+
+
+        Ii = I_next
+        Ui = Uc_next
+
+
+
---
+++ Lab 2./methods.py
@@ -0,0 +1,50 @@
+def Runge_Kutta_IV_3var_1step(f, phi, x0, y0, z0, h):
+    k1 = f(x0, y0, z0)
+    p1 = phi(x0, y0, z0)
+
+    k2 = f(x0 + h/2, y0 + k1 * h / 2, z0 + p1 * h / 2)
+    p2 = phi(x0 + h/2, y0 + k1 * h / 2, z0 + p1 * h / 2)
+
+    k3 = f(x0 + h / 2, y0 + k2 * h / 2, z0 + p2 * h / 2)
+    p3 = phi(x0 + h / 2, y0 + k2 * h / 2, z0 + p2 * h / 2)
+
+    k4 = f(x0 + h / 2, y0 + k3 * h, z0 + p3 * h)
+    p4 = phi(x0 + h / 2, y0 + k3 * h, z0 + p3)
+
+    yn = y0 + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+    zn = z0 + h / 6 * (p1 + 2 * p2 + 2 * p3 + p4)
+
+    return yn, zn
+
+
+def simpson(func, left, right, n, *args):
+    h = (right - left) / n
+
+    ans = h / 3
+    even = 0.0
+    odd = 0.0
+
+    for i in range(1, n):
+        if i % 2 == 0:
+            even += func(left + h * i, *args)
+        else:
+            odd += func(left + h * i, *args)
+
+    ans *= (2 * even + 4 * odd + func(left, *args) + func(right, *args))
+    return ans
+
+
+def dichotomy(func, left, right, eps, *args):
+    while abs(right - left) > eps:
+        c = (left + right) / 2.0
+
+        if func(right, *args) * func(c, *args) <= 0:
+            left = c
+        else:
+            right = c
+    return (right + left) / 2.0
+
+
+def newton_interpolation():
+    pass
+
